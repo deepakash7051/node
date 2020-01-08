5ce570fc5a035ce14e13e4222de9319ea4d7c710
Your Name <deepakash7051@gmail.com>
2020-01-09 00:12:20 +0530

    custom module

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 //const getNotes = require('./notes.js')
 const name = require('./notes.js')
+const sum = require('./utilis.js')
 //fs.writeFileSync('text.txt','\nnode is awesome');
-fs.appendFileSync('text.txt','\nAkashdeep');
+//fs.appendFileSync('text.txt','\nAkashdeep');
 //const msg = getNotes();
+const add = sum(5,8);
 console.log(name);
+console.log(add);
