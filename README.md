ba18ee4fe8062df91c1f9833e7e7de5e3c06e300
Your Name <deepakash7051@gmail.com>
2020-01-09 00:36:38 +0530

    custom module

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node
+initialize npm into our project:
+	npm init
+Search modules:
+	www.npmjs.com // search validator
+install npm module:
+	npm i validator@10.8.0
--- notesApp/app.js
+++ notesApp/app.js
@@ -2,9 +2,12 @@ const fs = require('fs')
 //const getNotes = require('./notes.js')
 const name = require('./notes.js')
 const sum = require('./utilis.js')
+const validator = require('validator')
 //fs.writeFileSync('text.txt','\nnode is awesome');
 //fs.appendFileSync('text.txt','\nAkashdeep');
 //const msg = getNotes();
 const add = sum(5,8);
+
 console.log(name);
 console.log(add);
+console.log(validator.isEmail('www@njbnj.com'))
